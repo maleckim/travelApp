1980fe1009361c11e0036ec9d68e2d524628a90c
abby krause <abbykrause@MacBook-Pro-3.local>
2019-11-16 19:28:38 -0600

    checking cors

---
+++ helper.js
@@ -0,0 +1,56 @@
+function userCredentials(){
+    $('.create').on('click',function(e){
+        event.preventDefault();
+        let userName = $('input[class = "usr"]').val();
+        let passWord = $('input[class = "pass"]').val();
+        createAccount(userName, passWord);
+        $('.usr').hide();
+        $('.pass').hide();
+        $('.local').show();
+        
+    });
+};
+
+
+function createAccount(userName, passWord){
+    localStorage.setItem('name',`${userName}`,'password',`${passWord}`);   
+}
+
+function addPlace(){
+    $('.create').on('click',function(e){
+        event.preventDefault();
+        let local = $('input[class = "local"]').val();
+        localStorage.setItem('location',`${local}`);
+        console.log(localStorage);
+    });
+}
+
+
+
+
+
+
+function dom(){
+    $(userCredentials);
+    $(addPlace);
+}
+
+$(dom);
+
+http://partners.api.skyscanner.net/apiservices/autosuggest/v1.0/Us/USD/en-US/?query=chicago&apiKey=prtl6749387986743898559646983194
+
+function getAirportsByCity(city){
+    fetch(`http://partners.api.skyscanner.net/apiservices/autosuggest/v1.0/Us/USD/en-US/?query=${city}&apiKey=prtl6749387986743898559646983194`)
+    .then(response => {
+        if(response.ok){
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(error => alert(`${error}`));
+}
+
+function displayResults(responseJson){
+    console.log(responseJson.Places.PlaceName);
+}
---
+++ index.js
@@ -0,0 +1,53 @@
+function getStarted(){
+    $('.start').on('click', function(e){
+        $('.main').hide();
+        $('.top').hide();
+        $('.getStarted').show();
+    })
+
+}
+
+function pullCity(){
+    $('.cityEnter').on('click', function(e){
+        let city = $('input[class = city]').val();
+        
+        getAirportsByCity(city);
+    })
+}
+
+function getAirportsByCity(city){
+    const token = "_eyW-HBGj4Fm08atyrx_YRrq7QP6Qn8Ro2uVCxmrlPCMGw_A-7jATd9jUKmE0HFS_6Nbn4BUKaXA459VZNP3hjQ=="
+    const options = {
+        headers: new Headers({
+            
+            "Access-Control-Allow-Origin": "*",
+            "Vary": "Origin"
+
+        })
+    };
+
+    fetch(`http://partners.api.skyscanner.net/apiservices/autosuggest/v1.0/Us/USD/en-US/?query=${city}&apiKey=_eyW-HBGj4Fm08atyrx_YRrq7QP6Qn8Ro2uVCxmrlPCMGw_A-7jATd9jUKmE0HFS_6Nbn4BUKaXA459VZNP3hjQ==`,options)
+    .then(response => {
+        if(response.ok){
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(error => alert(`${error}`));
+}
+
+function displayResults(responseJson){
+    console.log(responseJson.Places.PlaceName);
+}
+
+
+
+
+
+function dom(){
+    $(getStarted);
+    $(pullCity);
+}
+
+$(dom);
