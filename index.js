c5cb536f1b38d7e3a1e2b8b5a9667cac3414ac5a
abby krause <abbykrause@MacBook-Pro-3.local>
2019-11-16 21:21:30 -0600

    unable to display

--- index.js
+++ index.js
@@ -23,24 +23,20 @@ function pullCity(){
 //         }
 //         throw new Error('unable to generate token');
 //     })
-//     .then(responseJson => x = responseJson )
+//     .then(responseJson => return(responseJson) )
 //     .catch(error => alert(`${error}`))
 // }
 
 function getAirportsByCity(city){
-    const token = "_eyW-HBGj4Fm08atyrx_YRrq7QP6Qn8Ro2uVCxmrlPCMGw_A-7jATd9jUKmE0HFS_6Nbn4BUKaXA459VZNP3hjQ=="
-    const options = {
-        headers: new Headers({
-            'Access-Control-Allow-Origin': 'no-cors',
-            "Accept": "application/json",
-            "Vary": "Origin",
+    // const token = generateToken();
+    
 
-        })
-    };
-
-    fetch(`https://partners.api.skyscanner.net/apiservices/autosuggest/v1.0/Us/USD/en-US/?query=${city}&apiKey=prtl6749387986743898559646983194`,{
+    fetch(`https://partners.api.skyscanner.net/apiservices/autosuggest/v1.0/Us/USD/en-US/?query=${city}&apiKey=_qDrc-5-DkJ-nLtxlp-dFyRvzKHpAJ9bqaq3Smkrwitjvt5_E_3VLEOVRdHIMl7QtBkNxci1xSCdXslu0gOZw2g%3D%3D`,{
         mode:'no-cors',
-        accept: 'application/json'
+        headers: new Headers({
+            'Accept': 'application/json'
+        })
+        
     })
     .then(response => {
         if(response.ok){
@@ -49,11 +45,11 @@ function getAirportsByCity(city){
         throw new Error(response.statusText);
     })
     .then(responseJson => displayResults(responseJson))
-    .catch(error => alert(`${error}`));
+    .catch(error => alert('nope'));
 }
 
 function displayResults(responseJson){
-    console.log(responseJson);
+     console.log(responseJson.Places);
 }
 
 
@@ -63,7 +59,7 @@ function displayResults(responseJson){
 function dom(){
     $(getStarted);
     $(pullCity);
-    $(getAirportsByCity);
+    
 }
 
 $(dom);
